be2422c2025cf6048cd717338076aaa3d8e0036c
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-05-17 12:05:14 +0100

    make google-map works

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from 'react'
+import GoogleMap from 'google-map-react'
 
 import './App.css'
 
+const AnyReactComponent = ({ text }) => (
+  <div style={{
+    color: 'white',
+    background: 'red',
+    padding: '15px 10px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    transform: 'translate(-50%, -50%)'
+  }}>
+    {text}
+  </div>
+)
+
 class App extends Component {
-  firstMethod(){
-    console.log('irstr')
+  constructor(props){
+    super(props)
+    this.center = {
+      lat: 46.41,
+      lng: 11.84
+    }
+    this.zoom = 6
   }
-  render(){
-    return <div className="appcontainer">First react</div>
+  render() {
+    return (
+      <div className="gm-container">
+        <GoogleMap
+          defaultCenter={this.center}
+          defaultZoom={this.zoom}
+        >
+          <AnyReactComponent
+            lat={46.4102}
+            lng={11.8440}
+            text={'Dolomites'}
+          />
+          <AnyReactComponent
+            lat={46.4202}
+            lng={12.8440}
+            text={'Dolomites'}
+          />
+        </GoogleMap>
+      </div>
+    )
   }
 }
 
